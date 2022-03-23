9cbd8740530ab3ed083dc00d380815d3f2c48ae6
Andrew Ni <niandrewdev@gmail.com>
2022-03-23 01:44:19 -0700

    initial commit
    Signed-off-by: Andrew Ni <niandrewdev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,58 @@
-import { ApolloServer } from 'apollo-server-express';
+// import * as fs from 'fs';
+// import * as https from 'https';
+import * as paypal from '@paypal/checkout-server-sdk';
+import * as dotenv from 'dotenv';
 import * as express from 'express';
-import * as session from 'express-session';
-import * as fs from 'fs';
-import * as https from 'https';
-import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import { resolvers } from './resolvers';
-import { typeDefs } from './typeDefs';
 
-const startServer = async (port: number) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }: any) => ({ req }),
+dotenv.config();
+const app = express();
+const port = 4000;
+
+const Environment =
+  process.env.NODE_ENV === 'production' ? paypal.core.LiveEnvironment : paypal.core.SandboxEnvironment;
+const paypalClient = new paypal.core.PayPalHttpClient(
+  new Environment(process.env.PAYPAL_CLIENT_ID as string, process.env.PAYPAL_CLIENT_SECRET as string)
+);
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(req, res);
+  res.render('index', {
+    paypalClientId: process.env.PAYPAL_CLIENT_ID,
   });
+});
 
-  await createConnection();
+app.post('/create-order', async (req, res) => {
+  console.log('CREATE ORDER');
+  console.log(paypalClient, req, res);
+});
 
-  const app = express();
-  app.set('trust proxy', process.env.NODE_ENV !== 'production');
-  app.use(
-    session({
-      resave: false,
-      saveUninitialized: false,
-      secret: 'asdfasdfasdfasdf',
-      cookie: {
-        sameSite: 'none',
-        secure: true,
-      },
-    })
-  );
 
-  await server.start();
-  server.applyMiddleware({
-    app,
-    cors: {
-      credentials: true,
-      // origin: 'https://studio.apollographql.com',
-      origin: 'https://localhost:3000',
-    },
-  });
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}`);
+});
 
-  // app.listen({ port: port }, () =>
-  //   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  // );
+// const startServer = async (port: number) => {
 
-  const CERT_PATH = '/home/andrew/mkcert/localhost+2.pem';
-  const KEY_PATH = '/home/andrew/mkcert/localhost+2-key.pem';
-  https
-    .createServer(
-      {
-        cert: fs.readFileSync(CERT_PATH, 'utf-8'),
-        key: fs.readFileSync(KEY_PATH, 'utf-8'),
-      },
-      app
-    )
-    .listen(port, () => {
-      console.log('server up');
-    });
-};
+//   const app = express();
+//   app.set('trust proxy', process.env.NODE_ENV !== 'production');
 
-startServer(4000);
+//   // app.listen({ port: port }, () =>
+//   //   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+//   // );
+
+//   const CERT_PATH = '/home/andrew/mkcert/localhost+2.pem';
+//   const KEY_PATH = '/home/andrew/mkcert/localhost+2-key.pem';
+//   https
+//     .createServer(
+//       {
+//         cert: fs.readFileSync(CERT_PATH, 'utf-8'),
+//         key: fs.readFileSync(KEY_PATH, 'utf-8'),
+//       },
+//       app
+//     )
+//     .listen(port, () => {
+//       console.log('server up');
+//     });
+// };
+
+// startServer(4000);
