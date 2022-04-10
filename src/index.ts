8edc5acc68cea76ff6a00520c783a6ea68c4c198
Andrew Ni <niandrewdev@gmail.com>
2022-04-10 02:24:18 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ dotenv.config();
 const app = express();
 const port = 4000;
 
-app.use(function(_, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (_, res, next) {
+  res.header('Access-Control-Allow-Origin', 'localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
@@ -21,15 +21,13 @@ const paypalClient = new paypal.core.PayPalHttpClient(
 );
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req, res);
   res.render('index', {
     paypalClientId: process.env.PAYPAL_CLIENT_ID,
   });
 });
 
 app.get('/ping', (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send('hello');
+  res.send({ data: 'hello' });
 });
 
 app.post('/create-order', async (req, res) => {
@@ -40,29 +38,3 @@ app.post('/create-order', async (req, res) => {
 app.listen(port, () => {
   console.log(`🚀 Server ready at http://localhost:${port}`);
 });
-
-// const startServer = async (port: number) => {
-
-//   const app = express();
-//   app.set('trust proxy', process.env.NODE_ENV !== 'production');
-
-//   // app.listen({ port: port }, () =>
-//   //   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-//   // );
-
-//   const CERT_PATH = '/home/andrew/mkcert/localhost+2.pem';
-//   const KEY_PATH = '/home/andrew/mkcert/localhost+2-key.pem';
-//   https
-//     .createServer(
-//       {
-//         cert: fs.readFileSync(CERT_PATH, 'utf-8'),
-//         key: fs.readFileSync(KEY_PATH, 'utf-8'),
-//       },
-//       app
-//     )
-//     .listen(port, () => {
-//       console.log('server up');
-//     });
-// };
-
-// startServer(4000);
